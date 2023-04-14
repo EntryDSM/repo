61b79191d9137d3c859249fb1a089ddbb5646749
ipiyou <tao710803@gmail.com>
2023-04-14 10:18:00 +0900

    feat: 로그인 유형 모달 추가

--- service/main/src/components/login-branch-modal/index.tsx
+++ service/main/src/components/login-branch-modal/index.tsx
@@ -1,6 +1,8 @@
 import ReactOutSideClickHandler from "react-outside-click-handler";
 import { User, Teacher, Close } from "@packages/ui/assets";
 import { Logo } from "@packages/ui";
+import { ReactNode } from "react";
+import Link from "next/link";
 
 interface Props {
   closeModal: () => void;
@@ -9,17 +11,47 @@ interface Props {
 export const LoginBranchModal = ({ closeModal }: Props) => {
   return (
     <div
-      className="flex items-center justify-center fixed top-0 right-0 left-0 h-[100vh]"
-      style={{ background: "rgba(0, 0, 0, 0.1)" }}
+      className="fixed top-0 right-0 left-0 h-[100vh] flex items-center justify-center "
+      style={{ background: "rgba(0, 0, 0, 0.7)" }}
     >
       <ReactOutSideClickHandler onOutsideClick={closeModal}>
-        <div className="bg-white w-[550px] h-[330px] rounded-lg">
-          <Logo />
-          <Close />
-          <User />
-          <Teacher />
+        <div className="w-[550px] h-[370px] relative rounded-lg bg-gray-100 flex flex-col items-center justify-center gap-y-10 px-20">
+          <div className="flex flex-col items-center gap-y-2.5">
+            <Logo />
+            <div className="body6">로그인 유형을 선택해 주세요</div>
+          </div>
+          <div className="flex flex-col gap-y-[15px] w-full">
+            <NavigateButton icon={<User size={30} />}>
+              학생 로그인하기
+            </NavigateButton>
+            <NavigateButton icon={<Teacher size={30} />}>
+              선생님 로그인하기
+            </NavigateButton>
+          </div>
+          <div className="absolute top-[30px] right-[30px] cursor-pointer">
+            <Close size={24} />
+          </div>
         </div>
       </ReactOutSideClickHandler>
     </div>
+  );
+};
+
+interface NaviType {
+  icon: ReactNode;
+  children: ReactNode;
+  to?: string;
+}
+
+const NavigateButton = ({ icon, children, to = "" }: NaviType) => {
+  return (
+    <Link href={to}>
+      <div className="w-full h-[50px] flex items-center bg-gray-50 gap-x-5 cursor-pointer hover:[*>div]:first:bg-gray-200">
+        <div className="w-[80px] h-full flex items-center justify-center bg-gray-900 rounded-s-md ">
+          {icon}
+        </div>
+        <div className="text-body5">{children}</div>
+      </div>
+    </Link>
   );
 };
