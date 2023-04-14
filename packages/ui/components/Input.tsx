9286f9d007ecf424702e1f2f0d779d9223aab105
ipiyou <tao710803@gmail.com>
2023-04-14 20:02:48 +0900

    fix: input password버그, icon추가, classname추가

--- packages/ui/components/Input.tsx
+++ packages/ui/components/Input.tsx
@@ -1,10 +1,12 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, ReactNode, useState } from "react";
 import { Preview } from "../assets/Preview";
 import { NonPreview } from "../assets";
 
 interface PropsType {
+  className?: string;
   kind?: "text" | "password" | "button";
   times?: string;
+  activeIcon?: ReactNode;
   name?: string;
   label?: string;
   value: number | string;
@@ -15,11 +17,13 @@ interface PropsType {
 }
 
 export const Input = ({
+  className,
   kind = "text",
   times,
   label,
   name,
   value,
+  activeIcon,
   placeholder,
   onChange,
   errorMsg,
@@ -31,16 +35,17 @@ export const Input = ({
 
   const borderError = errorMsg ? "error" : "gray900";
   const borderFocus = kind === "button" ? "" : "focus-within:border-blue";
-  const isPassword = kind === "password" && !open;
+  const isPassword = kind === "password";
+  const isOpenText = !open ? "password" : "text";
   return (
-    <div className="relative">
+    <div className={"relative" + className}>
       <div className="text-body8 mb-2.5 ml-[7px]">{label}</div>
       <div className="absolute top-[1px] right-[7px] text-body7">{times}</div>
       <div
         className={`flex items-center bg-gray50 rounded border-2 h-[46px] border-${borderError} pl-5 ${borderFocus}`}
       >
         <input
-          type={isPassword ? "password" : "text"}
+          type={isPassword ? isOpenText : "text"}
           name={name}
           value={value}
           placeholder={placeholder}
@@ -54,11 +59,14 @@ export const Input = ({
             {kind}
           </div>
         )}
-        {isPassword && (
-          <div onClick={PasswordTypeChange} className="">
-            <EyeIcon size={24} />
-          </div>
-        )}
+        <div className="mr-5">
+          {isPassword && (
+            <div onClick={PasswordTypeChange}>
+              <EyeIcon size={24} />
+            </div>
+          )}
+          {activeIcon}
+        </div>
       </div>
       {successMsg && <div className="text-body7 text-green">{successMsg}</div>}
       {errorMsg && <div className="text-body7 text-error">{errorMsg}</div>}
