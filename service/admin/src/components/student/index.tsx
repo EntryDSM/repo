9748696bdaf6a3b1e02bf2fca416a784b9688fb2
Josanghyeon <BStkdgus.dev@gmail.com>
2023-04-15 15:06:20 +0900

    fix: arrow 방향 버그 수정

--- service/admin/src/components/student/index.tsx
+++ service/admin/src/components/student/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export const Student = () => {
   return (
     <Link href={"/"}>
-      <article className="flex w-[1120px] h-20 pl-10 pr-10 items-center bg-gray50">
+      <article className="flex w-[1120px] h-20 pl-10 pr-10 items-center bg-gray50 rounded-md">
         <Image width={46} height={46} src={dummy} alt="" />
         <div className="w-28 text-body4 ml-5">좌찬익</div>
         <div className="text-body4 w-96">3218</div>
