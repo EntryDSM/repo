b37e4845a4881f2792185a660823b394df14951c
ipiyou <tao710803@gmail.com>
2023-05-03 12:30:04 +0900

    feat: 전공 추가 삭제 검색 api 연동

--- service/admin/src/pages/technology/index.tsx
+++ service/admin/src/pages/technology/index.tsx
@@ -1,30 +1,31 @@
 import { Header } from "@/components/header";
 import { Button, Input } from "@packages/ui";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Plus, Search } from "@packages/ui/assets";
 import { Tag } from "@/components/technology/Tag";
-import {
-  MajorCreate,
-  createMajor,
-  searchMajor,
-  deleteMajor,
-} from "@/apis/major";
+import { getMajor, postMajor, deleteMajor } from "@/apis/major";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "react-toastify";
 
 const technology = () => {
   const [searchTechnology, setSearchTechnology] = useState("");
   const [technology, setTechnology] = useState("");
+  const [newMajor, setNewMajor] = useState<boolean>(true);
 
-  const { data } = useQuery(["dq"], searchMajor);
+  const { data } = useQuery(["dq"], getMajor, {
+    onSuccess: () => setNewMajor(false),
+    onError: () => setNewMajor(false),
+    enabled: newMajor,
+  });
 
-  const { mutate } = useMutation({
-    mutationFn: (body: string) => createMajor(body),
+  const { mutate: create } = useMutation({
+    mutationFn: (body: string) => postMajor({ major_name: body }),
     onSuccess: (res) => {
       toast("성공적으로 생성하였습니다.", {
         autoClose: 1000,
         type: "success",
       });
+      setNewMajor(true);
     },
     onError: (err) => {
       toast("무언가 잘못되었습니다!", {
@@ -41,6 +42,7 @@ const technology = () => {
         autoClose: 1000,
         type: "success",
       });
+      setNewMajor(true);
     },
     onError: (err) => {
       toast("무언가 잘못되었습니다!", {
@@ -53,6 +55,16 @@ const technology = () => {
   const deleteTag = (uuid: string) => {
     del(uuid);
   };
+
+  const addMajor = (e: FormEvent) => {
+    e.preventDefault();
+    create(technology);
+    setTechnology("");
+  };
+
+  const filterdList = data?.data.major_list.filter((major) =>
+    major.name.includes(searchTechnology)
+  );
 
   return (
     <div>
@@ -70,7 +82,7 @@ const technology = () => {
             placeholder="검색할 전공을 입력해주세요"
             onChange={(e) => setSearchTechnology(e.target.value)}
           />
-          <div className="flex">
+          <form className="flex" onSubmit={addMajor}>
             <Input
               value={technology}
               kind="custom"
@@ -78,22 +90,21 @@ const technology = () => {
               placeholder="추가할 전공을 입력해주세요"
               onChange={(e) => setTechnology(e.target.value)}
             />
-            <Button kind="outline" onClick={() => mutate(technology)}>
+            <Button kind="outline">
               <Plus size={18} />
               전공 추가
             </Button>
-          </div>
+          </form>
         </div>
 
         <div className="flex gap-2 flex-wrap">
-          <span className="text-body5">전체</span>
-          <span className="text-gray300">16</span>
+          <span className="text-body5">{searchTechnology || "전체"}</span>
+          <span className="text-gray300">{filterdList?.length}</span>
         </div>
         <div className="flex gap-3 mt-5 flex-wrap">
-          {data &&
-            data?.data.major_list.map((major) => (
-              <Tag onClick={deleteTag} data={major} />
-            ))}
+          {filterdList?.map((major) => (
+            <Tag onClick={deleteTag} data={major} />
+          ))}
         </div>
       </div>
     </div>
