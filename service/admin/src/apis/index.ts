b4ac7d3d345b8f8beebf0a0742a422f792a08a02
ipiyou <tao710803@gmail.com>
2023-06-02 09:04:23 +0900

    feat: 유저, 어드민 로그인 안되면 메인페이지로

--- service/admin/src/apis/index.ts
+++ service/admin/src/apis/index.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosError } from "axios";
-import { useRouter } from "next/router";
 
 export const instance = axios.create({
   baseURL: "https://api.dsm-repo.com",
@@ -8,18 +7,34 @@ export const instance = axios.create({
 
 instance.interceptors.request.use(
   async function (config) {
-    const { push } = useRouter();
     const accessToken = localStorage.getItem("access_token");
     if (accessToken) {
       // @ts-ignore
       config.headers = {
         Authorization: `Bearer ${accessToken}`,
       };
-    } else push("https://www.dsm-repo.com/");
-
+    }
     return config;
   },
   function (error: AxiosError) {
+    error.status;
+    if (error.status) {
+      window.location.href = "https://www.dsm-repo.com/";
+    }
     return Promise.reject(error);
   }
 );
+
+instance.interceptors.response.use(undefined, function (error) {
+  const status = error.response?.status;
+  const isSignUp = window.location.href.includes("/sign-in");
+  if (status && !isSignUp) {
+    switch (error.response.status) {
+      case 401:
+        window.location.href = "https://www.dsm-repo.com/";
+        break;
+      default:
+        return Promise.reject(error);
+    }
+  }
+});
--- service/user/src/apis/index.ts
+++ service/user/src/apis/index.ts
@@ -13,17 +13,30 @@ export const instance = axios.create({
 
 instance.interceptors.request.use(
   async function (config) {
-    const { push } = useRouter();
     const accessToken = localStorage.getItem("access_token");
     if (accessToken) {
       //@ts-ignore
       config.headers = {
         Authorization: `Bearer ${accessToken}`,
       };
-    } else push("https://www.dsm-repo.com/");
+    }
     return config;
   },
   function (error: AxiosError) {
     return Promise.reject(error);
   }
 );
+
+instance.interceptors.response.use(undefined, function (error) {
+  const status = error.response.status;
+  const isSignIn = window.location.href.includes("/sign-in");
+  if (status && !isSignIn) {
+    switch (status) {
+      case 401:
+        window.location.href = "https://www.dsm-repo.com/";
+        break;
+      default:
+        return Promise.reject(error);
+    }
+  }
+});
