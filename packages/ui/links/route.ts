95a9921eb771e91263543f42ac4ab8231af9c949
ipiyou <tao710803@gmail.com>
2023-06-07 22:45:49 +0900

    feat: 테스트,배포 환경에서 링크 이동

--- packages/ui/links/route.ts
+++ packages/ui/links/route.ts
@@ -1,5 +1,12 @@
-export const pageRouteLinks = {
-  main: "http://localhost:3000",
-  user: "http://localhost:3001",
-  admin: "http://localhost:3002",
-};
+export const pageRouteLinks = (() =>
+  window.location.href.startsWith("http://localhost:")
+    ? {
+        main: "http://localhost:3000",
+        user: "http://localhost:3001",
+        admin: "http://localhost:3002",
+      }
+    : {
+        main: "https://www.dsm-repo.com/",
+        user: "https://user.dsm-repo.com/",
+        admin: "https://teacher.dsm-repo.com/",
+      })();
