24208aa266dc516ac742d671fa61edd97f90a7b8
jisoung <jangjisoung0405@gmail.com>
2023-07-11 12:21:39 +0900

    fix : react buddy 제거

--- service/admin/src/pages/_app.tsx
+++ service/admin/src/pages/_app.tsx
@@ -3,8 +3,6 @@ import type {AppProps} from "next/app";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ToastClassName, ToastContainer, TypeOptions} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {DevSupport} from "@react-buddy/ide-toolbox-next";
-import {ComponentPreviews, useInitial} from "@/components/dev";
 
 const client = new QueryClient();
 
@@ -12,11 +10,7 @@ export default function App({Component, pageProps}: AppProps) {
     return (
         <div className=" [&_*]:font-['Pretendard-Regular'] [&_*]:leading-tight">
             <QueryClientProvider client={client}>
-                <DevSupport ComponentPreviews={ComponentPreviews}
-                            useInitialHook={useInitial}
-                >
-                    <Component {...pageProps} />
-                </DevSupport>
+                <Component {...pageProps} />
                 <ToastContainer/>
             </QueryClientProvider>
         </div>
