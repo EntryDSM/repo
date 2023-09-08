d1a2adce67c2a1d699c3c3f5137a9808584f77b3
SeungWoo <wjknn3123@gmail.com>
2023-09-08 22:58:31 +0900

    fit : 필수 필드 toast 추가

--- service/user/src/apis/document/patch/Certificate.ts
+++ service/user/src/apis/document/patch/Certificate.ts
@@ -1,5 +1,6 @@
 import { disableId } from ".";
 import { instance } from "../..";
+import { toast } from "react-toastify";
 
 export interface CertificateReqBody {
   name: string;
@@ -13,7 +14,14 @@ export interface CertificateResType extends CertificateReqBody {
 }
 
 export const documnetCertificate = (body: CertificateReqBody[]) => {
-  return instance.patch("/document/certificate", {
-    certificate_list: body,
-  });
+  return instance
+    .patch("/document/certificate", {
+      certificate_list: body,
+    })
+    .catch((error) => {
+      toast("입력하지 않은 필드가 있습니다.", {
+        autoClose: 1000,
+        type: "error",
+      });
+    });
 };
