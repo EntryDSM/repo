d0973abbc66535f6aad4dc5af29cdcb1f9d241ae
SeungWoo <wjknn3123@gmail.com>
2023-09-18 21:47:58 +0900

    fix : project skill wrap

--- packages/ui/components/PreviewResume/Tag.tsx
+++ packages/ui/components/PreviewResume/Tag.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Tag = ({ technology, className }: Props) => (
   <div
-    className={`${className} rounded-md pl-2 pr-2 pt-0.5 pb-0.5 text-body7 flex items-center justify-center`}
+    className={`${className} rounded-md pl-2 pr-2 pt-0.5 pb-0.5 text-body7 flex items-center justify-center whitespace-nowrap`}
   >
     {technology}
   </div>
