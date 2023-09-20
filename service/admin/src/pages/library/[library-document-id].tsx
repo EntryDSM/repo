1b86fc210c2cd8e0acedb89ff683a795f7edda56
SeungWoo <wjknn3123@gmail.com>
2023-09-20 13:16:44 +0900

    fix : temporary url type

--- service/admin/src/pages/library/[library-document-id].tsx
+++ service/admin/src/pages/library/[library-document-id].tsx
@@ -13,11 +13,11 @@ const Library = () => {
     { enabled: !!libraryId }
   );
   const { data: document } = useQuery(
-      ["documentListadmin"],
-      () => getLibrary(""),
-      {
-        enabled: !!libraryId,
-      }
+    ["documentListadmin"],
+    () => getLibrary(""),
+    {
+      enabled: !!libraryId,
+    }
   );
 
   const list = student?.data.index;
@@ -26,6 +26,21 @@ const Library = () => {
     ({ id }) => id === libraryId
   )?.document_url;
 
-  return <PdfViewer list={list} url={url} />;
+  return (
+    <PdfViewer
+      list={list?.map((value) => {
+        return {
+          name: value.name,
+          student_number: value.student_number,
+          major: {
+            id: value.major,
+            name: value.major,
+          },
+          page: value.page,
+        };
+      })}
+      url={url}
+    />
+  );
 };
 export default Library;
